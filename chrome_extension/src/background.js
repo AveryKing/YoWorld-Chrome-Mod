e16756438d02bfeaf8fc02589cc25b9e36943c9e
Avery King <avery.n.king@wmich.edu>
2022-01-06 13:39:25 -0500

    chrome extension added

--- chrome_extension/src/background.js
+++ chrome_extension/src/background.js
@@ -1,12 +1,3 @@
-/*
-chrome.runtime.onMessage.addListener(({ action, data }, sender, sendResponse) => {
-  /* if (action === 'inject') {
-        chrome.tabs.executeScript(sender.tab.id, {
-            code: getInjectionCode(data),
-            frameId: sender.frameId,
-        });
-    }
-    if(action === 'chat') {
-        console.log('chat message sent');
-    }
-});*/
+top.window.addEventListener("message", function(message) {
+    console.log(`message received ${message}`);
+});
--- chrome_extension/src/inject/inject.js
+++ chrome_extension/src/inject/inject.js
@@ -7,7 +7,6 @@ chrome.extension.sendMessage({}, function(response) {
   		// This part of the script triggers when page is done loading
         chrome.runtime.onMessage.addListener(
             function(request, sender, sendResponse) {
-
                 if(request.command === 'sendChatMessage') {
                     var yoFrame = document.getElementById('iframe_canvas').contentWindow;
                     yoFrame.postMessage('sendChatMessage', '*');
@@ -19,45 +18,6 @@ chrome.extension.sendMessage({}, function(response) {
                     sendResponse({farewell: "goodbye"});
             }*/
         );
-  		// ----------------------------------------------------------
-
-      var checkChange = function(){
-        var flag = false;
-        var names = document.querySelectorAll('.item-name-container');
-        var forEach = Array.prototype.forEach;
-        forEach.call(names, function(nameItem){
-          if(nameItem.getAttribute('name') === 'test') {
-            flag = true;
-          }
-        });
-        window.parent.postMessage(flag + '', '*');
-        console.log('flag: ', flag);
-      };
-
-
-       // var yoFrame = document.getElementById('iframe_canvas').contentWindow;
-
-          //  yoFrame.postMessage('sendChatMessage', '*');
-
-      // select the target node
-      var target = document.querySelector('body');
-
-      // create an observer instance
-      var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-          // console.log(mutation.type);
-          checkChange();
-        });
-      });
-
-      // configuration of the observer:
-      var config = { attributes: true, childList: true, characterData: true };
-
-      // pass in the target node, as well as the observer options
-      observer.observe(target, config);
-
-      // later, you can stop observing
-      // observer.disconnect();
 
   	}
 	}, 10);
