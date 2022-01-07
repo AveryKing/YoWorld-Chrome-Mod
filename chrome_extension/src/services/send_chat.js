a553415a8f6047f5bc007dead7aef708ec3f6117
Avery King <avery.n.king@wmich.edu>
2022-01-06 21:49:15 -0500

    chrome extension added

--- chrome_extension/src/services/send_chat.js
+++
@@ -1,8 +0,0 @@
-//noinspection JSUnresolvedVariable,JSUnresolvedFunction
-
-export default async function sendChat(yoFrame, chatMessage) {
-    await yoFrame.evaluate((chatMessage) => {
-        MyLife.MyLifeInstance.getInstance().getServer()
-            .sendPublicMessage(MyLife.Chat.encodePublicMessage(chatMessage), 0)
-    }, chatMessage)
-}
