04f3f80ee345f32124212791d48e69bfbb96a943
Avery King <avery.n.king@wmich.edu>
2022-01-07 21:16:33 -0500

    chrome extension fully functional now (no fiddler required)

--- README.md
+++
@@ -1,2 +0,0 @@
-# YoWorld-Bot
-Node.js bot for YoWorld
--- actions/translate.js
+++
@@ -1,9 +0,0 @@
-import google from 'translate-google';
-
-export default async function translate(text, language) {
-    return new Promise((resolve) => {
-        google(text, {to: language})
-            .then(translatedText => resolve(translatedText)
-            );
-    })
-}
