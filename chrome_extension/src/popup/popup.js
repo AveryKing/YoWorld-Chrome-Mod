8cb6195de1ddc5d6b920be1d900a411f9759efb8
Avery King <avery.n.king@wmich.edu>
2022-01-10 04:50:30 -0500

    refactor

--- chrome_extension/src/popup/popup.js
+++ chrome_extension/src/popup/popup.js
@@ -1,16 +1,16 @@
 let display = 'main';
 let selected = 0;
 let outfitItems = {};
-let outfitImages = document.getElementById("outfitImages");
-let itemIdArr = [];
-let selectedOutfits = [];
+
 chrome.runtime.onMessage.addListener(
     async (response, sender, sendResponse) => {
         if (response.cmd === 'setupOutfitSwitcher') {
             let outfitData = response.data;
             outfitData.forEach(outfit => {
+                    let itemIdArr = [];
                     itemIdArr.push(outfit.items)
                     outfitItems[outfit.outfitId] = itemIdArr;
+                    console.log(outfitItems)
                     let img = document.createElement("img");
                     img.src = outfit.image;
                     img.alt = outfit.outfitId;
@@ -18,30 +18,29 @@ chrome.runtime.onMessage.addListener(
                     img.style.width = '75%';
                     let container = document.createElement("div");
                     container.classList.add("container");
-                    container.appendChild(img);
-                    outfitImages.appendChild(container)
+                    container.appendChild(img)
+                    document.getElementById("outfitImages").appendChild(container)
                     let div = document.createElement("div");
                     div.classList.add("middle");
                     div.onclick = () => {
                         if (selected > 0) {
-                            let containerClassList =  div.parentElement.classList
-                            containerClassList.remove('container')
-                            containerClassList.add('container-selected');
+                            div.parentElement.classList.remove('container');
+                            div.parentElement.classList.add('container-selected');
                             setTimeout(() => {
-                                fadeOut(outfitImages, 0);
+                                fadeOut(document.getElementById("outfitImages"), 0);
                                 fadeOut(document.getElementById("outfitText"), 0);
                                 fadeIn(document.getElementById("currentlySwitching"), 700);
                             }, 200);
-                            [].forEach.call(document.querySelectorAll('.container-selected'), (el) => {
+                            let selectedOutfits = [];
+                            [].forEach.call(document.querySelectorAll('.container-selected'), function (el) {
                                 let cln = el.cloneNode(true);
                                 let subArr = []
-                                let switchingOutfits = document.getElementById('switchingOutfits');
                                 subArr.push(outfitItems[el.childNodes[0].alt])
                                 selectedOutfits.push(subArr);
                                 cln.style.width = '150px';
                                 cln.classList.remove('container-selected');
-                                switchingOutfits.style.display = "inline-flex";
-                                switchingOutfits.appendChild(cln);
+                                document.getElementById("switchingOutfits").style.display = "inline-flex";
+                                document.getElementById("switchingOutfits").appendChild(cln);
                                 sessionStorage.setItem('switchingOutfits', 'true');
                             });
                             chrome.tabs.query({active: true, currentWindow: true}, async (tabs) => {
